c012a1987a907f7ff83e43382771ed254e4e654a
JhonatanGel146 <jhonatangelvis@gmail.com>
2022-11-22 22:28:40 -0500

    spotify

--- Python/Miselanias/Diccionario/musica.py
+++ Python/Miselanias/Diccionario/musica.py
@@ -15,38 +15,8 @@ el que tiene  las cancion mas larga
 el que tiene la cancion mas corta """
 
 
-listareproduccion={"maluma" :(
-                            "perdedor ", 
-                            "duracion " "3:27", 
-                            "genero " "reggaeton"
-                            ),
-                   "morat": (
-                            "cancion ""besos en guerra", 
-                            "duracion " "3:51" , 
-                            "genero " "romance"
-                            ),
-                    "INTERWORLD": (
-                                "cancion ""metamorfosis",
-                                "duracion " "2:22",
-                                "genero " "phonk"                                                                 
-                            ),
-                    "metallica": (
-                                "cancion " "enter sandmman",
-                                "duracion "  "5:31",
-                                "genero " "rock"
-                                ),
-                    "sebastian yatra": (
-                                "canciones ""ojos marrones",
-                                "duracion " "3:18",
-                                "genero ""urbano"
-                                ),
-                    "luis miguel": (
-                                "cancion ""ahora te puedes marchar",
-                                "duracion " "3:11",
-                                "genero ""pop"
-                    )
-                    
-                    }
+listareproduccion={}
+                   
               
 
 # funcion buscar
@@ -61,19 +31,21 @@ def artista (playlist):
 #funcion agregar artitas
 def agregarArtista(playlist):
         y=input("Escribe el nombre del artista que quieres agregar\n")
-        playlist[y]=()
+        playlist[y]={}
         print(playlist)
         return 1
     
 #Agregar cancion
 def agregarCancion (playlist):
     artiscan=input("Nombre del artista al que le quieres agregar una cancion:\n")
-    c=input("Nombre de la cancion que quieres agregar:\n")
-    g=input("genero de la cancion:\n")
-    d=input("duracion de la cancion:\n")
-    dici=("cancion", c,"duracion", d,"genero", g)
-    playlist[artiscan]+=dici
-    print(playlist)
+    if artiscan in playlist:
+        c=input("Nombre de la cancion que quieres agregar:\n")
+        g=input("genero de la cancion:\n")
+        d=int (input("minutos y segundos de  duracion de la cancion:\n"))
+        playlist[artiscan].update({'cancion':c,'genero':g,'duracion':d})
+        print(playlist)
+    else:
+        print("Agrega al artista primero")
 #Eliminar artista
 def eliminarArtista(playlist):
     eliminar=input("Nombre del artista que quieres eliminar\n")
@@ -81,8 +53,27 @@ def eliminarArtista(playlist):
     print(playlist)
 #organizar playlist
 def ordenarLista(playlist):
-    sorted((playlist.keys()))
+    sorted(playlist.keys())
     print(playlist)
+#buscar cancion
+def buscar_cancion(playlist):
+    x=input("Nombre de la cancion que quieres buscar:\n")
+    for y in playlist.keys():
+        if x in playlist[y]["cancion"]:
+            print("Si esta")
+        else:
+            print("no esta")
+def mas_canciones(playlist):
+    for y  in playlist.keys(): 
+        cont=0
+        for x in playlist.values():
+            if x in playlist[y]["cancion"]:
+                cont+=1
+                if cont >= cont:
+                    print(playlist[y])
+                
+
+
 
 while True:
     print("1-Buscar artista")
@@ -90,26 +81,37 @@ while True:
     print("3-Agregar cancion")
     print("4-Agregar eliminar artista")
     print("5-organizar playlist")
-    print("6-Salir")
+    print("6-Buscar cancion")
+    print("7-Salir")
     ctrl=int(input("Selecciona la opcion: "))
     match ctrl:
         case 1:
             print("selecciono la opcion 1")
-            x= artista(listareproduccion)
-
+            artista(listareproduccion)
             print()
         case 2:
             print("selecciono la opcion 2")
             agregarArtista(listareproduccion)
+            print()
             
         case 3:
             print("selecciono la opcion 3")
             agregarCancion(listareproduccion)
+            print()
         case 4:
             print("selecciono la opcion 4")
             eliminarArtista(listareproduccion)
+            print()
         case 5:
             ordenarLista(listareproduccion)
+            print()
         case 6:
+            buscar_cancion(listareproduccion)
+            print()
+        case 7:
                 print("saliste")
                 break
+        case _:
+            print("opcion no disponible")
+            print()
+mas_canciones(listareproduccion)
