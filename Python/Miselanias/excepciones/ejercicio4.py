3d07b38b2ece274ff13c152ca5d5442293c3e087
JhonatanGel146 <jhonatangelvis@gmail.com>
2022-11-24 22:49:53 -0500

    juego

--- Python/Miselanias/excepciones/ejercicio4.py
+++ Python/Miselanias/excepciones/ejercicio4.py
@@ -1,22 +1,39 @@
 
-def ahorcado(palabra_secreta):
+def ahorcado():
+    palabra_secreta=str(input("ingresa la palabra secreta:\n"))
     lista = []
     for i in range(len(palabra_secreta)):
         lista.append(palabra_secreta[i])
-    print (lista)
+    lista2=[]
+    for k in range(len(palabra_secreta)):
+        lista2.append("-")
+    print(lista2)
     cont=0
     while cont <6:
-        letra=input("Letra que esta en la palabra.\n")
+        letra=input("Letra que esta en la palabra:\n")
         if letra in lista:
+            print("La letra si estaba en la palabra")
             for j in range(len(lista)):
-                if letra==j:
-                    del lista[j]
-                    print(lista)
-            print("si estaban en la palabra:")
+                if letra == lista[j]:
+                    lista2.insert(j,letra)
+                    del lista2 [j+1]
+                    print(lista2)
+            if lista2 == lista:
+                print("!Ganaste¡")
+                break
         else:
             print("No estaban en la palabra:")
             cont+=1
-            if cont ==6:
-                print("perdiste")
-palabra_secreta=input("ingresa la palabra secreta")
-ahorcado(palabra_secreta)
+            if cont ==1:
+                print(" *\n"*4,"******")
+            elif cont==2:
+                print(" *       *****\n"*4,"******  *****")
+            elif cont==3:
+                print(" *       ***** *****\n"*4,"******  ***** *****")
+            elif cont==4:
+                print(" *       ***** ***** *****\n *       ***** ***** *\n *       ***** ***** *****\n *       ***** *****      *\n *       ***** *****      *\n","******  ***** ***** *****")
+            elif cont==5:
+                print(" *       ***** ***** *****  *****\n *       ***** *****  *     *\n *       ***** ***** *****  *\n *       ***** *****      * *****\n *       ***** *****      * *\n","******  ***** ***** *****  *****")
+            elif cont==6:
+                print(" *       ***** ***** ***** *****  *****\n *       ***** ***** *      *     *   *\n *       ***** ***** *****  *     *   *\n *       ***** *****      * ***** *  *\n *       ***** *****      * *     * *\n","******  ***** ***** ***** *****  *  *")
+ahorcado()
--- Python/apuntes/re.py
+++ Python/apuntes/re.py
@@ -1,10 +1,46 @@
-l = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
-lista=[]
+
+#Comparar lista
+def lista_igual(lista1,lista2):  
+    if lista1 == lista2:
+        return("Son iguales")
+    else:
+        return("Son diferentes")
+#Insertar elemento a la lista 1
+def insertar(lista1):
+    x=int(input("Elemento que quieres agregar"))
+    lista1.append(x)
+    print("La lista esta asi:", lista1)
+    
+#Insertar elemento a la lista 2
+def insertar2(lista2):
+    y=int(input("Elemento que quieres agregar"))
+    lista2.append(y)
+    print("La lista esta asi:", lista2)
+    
 
 
-for i in range(len(l)):
-    if l[i] not in lista[:]:
-        lista.append(l[i])
 
-print("La lista con elementos únicos:")
-print(lista)
+
+
+lista1=[]
+lista2=[]
+while True:
+    print("Selecciona la opcion que quieres ejecutar\n")
+    print("1-Insertar un elemento a la primera lista")
+    print("2-Insertar un elemento a la segunda lista")
+    print("3-Comparar lista")
+    print("4-salir")
+    print()
+    control=int(input("Seleccione la opcion:\n"))
+    match control:
+        case 1:
+            insertar(lista1)
+        case 2:
+            insertar2(lista2)
+        case 3:
+            print(lista_igual(lista1,lista2))
+        case 4:
+            print("saliste")
+            break
+        case _:
+            print("NO HAY ESTA OPCION ")
