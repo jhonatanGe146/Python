6c38e0a796796b9b18fbd045490cf6a6b4908038
JhonatanGel146 <jhonatangelvis@gmail.com>
2023-03-07 20:30:03 -0500

    evaluacion

--- Python/Evaluaciones/Clases.py
+++ Python/Evaluaciones/Clases.py
@@ -5,12 +5,11 @@ class Material:
         self.Titulo_Material = titulo
         self.Tipo_Material = tipo
         self.Autor_Material = auto
-        
     def getTitulo(self):
         return self.Titulo_Material
     def setTitulo(self,titulo):
         self.Titulo_Material=titulo
-    #setter/getter Tipo_libro}
+    #setter/getter Tipo_libro
     def getTipo(self):
         return self.Tipo_Material
     def setTipo(self,tipo):
@@ -22,36 +21,34 @@ class Material:
         self.Autor_Material=autor
     
     
+    
         
 class libro(Material):
     def __init__(self,titulo,tipo,auto,edi):
         Material.__init__(self,titulo,tipo,auto)
         self.Editorial_libro=edi
+        self.__d=[self.Tipo_Material,self.Autor_Material,self.Editorial_libro]
     def getEditorial(self):
         return self.Editorial_libro
     def setEditorial(self,edi):
         self.Editorial_libro=edi
+    def Cont_Libro (self):
+        return self.__d
+    
    
 
 class Revista(Material):
     def __init__(self,titulo,tipo,auto,edi):
         Material.__init__(self,titulo,tipo,auto)
         self.Edicion=edi
+        self.__d=[self.Tipo_Material,self.Autor_Material,self.Edicion]
     def getEdicion(self):
         return self.Edicion
-    def setEditorial(self,edi):
+    def setEdicion(self,edi):
         self.Edicion=edi
+    def Cont_Revis (self):
+        return self.__d
        
-# y=libro('Cien Años de Soledad','Drama','Gabriel','NN')
-# print(y.getTitulo())
-# y.setTitulo('el principito')
-# print(y.getTitulo())
-# print(y.getEditorial())
-# x=Revista('chistes de la semana','comedia','perez','la risa')
-# print(x.getTitulo())
-# x.setTitulo('el principito')
-# print(x.getTitulo())
-# print(x.getEdicion())
 
 
 class Lector:
@@ -59,7 +56,7 @@ class Lector:
         self.Nombre = Nom
         self.__Direccion = Dir
         self.__Telefono = Te
-        self.__libro=[]
+        self.__reservado={}
         
     #setter/getter Nombre
     def getNombre(self):
@@ -72,35 +69,36 @@ class Lector:
     def setDireccion(self,direccion):
         self.__Direccion=direccion
     #setters/getter Telefono
-    def getTelefeno (self):
+    def getTelefono (self):
         return self.__Telefono
     def setTelefono (self, tele):
         self.__Telefono=tele
     #Reservar libro
-    def resevar(self, material):
-        self.__libro.append(material)
-    def getRervados(self):
-        for i in self.__libro:
-            print(i.getTitulo(),' ')
-
-
+    def reserva (self,No_t,material):
+        self.__reservado[No_t]=material
+    def getReserva (self):
+        return f'{self.Nombre} a reservado {self.__reservado}'
+    
+    #Devolver Libro:
+    def Entregar(self, titulo):
+        if titulo in self.__reservado:
+            print(f'{self.Nombre} a Entregado {titulo}')
+            del self.__reservado[titulo]
+        else:
+            print('No ha reservado este libro')
+   
         
     
-
-
-
 class Estudiante(Lector):
     __codigo=100
     def __init__(self,Nom,Dir,Te):
         Lector.__init__(self,Nom, Dir, Te)
         Estudiante.__codigo+=1
-        
 
     def getCodigoEstudiante(self):
         return Estudiante.__codigo
     
-# es=Estudiante('jhon','cra 12',321)
-# print(es.getCodigoEstudiante())
+
 
 class Docente(Lector):
     __codigo=100
@@ -111,15 +109,9 @@ class Docente(Lector):
     def getCodigoDocente(self):
         
         return Docente.__codigo
-    
-    
-# s=Docente('Samuel','cr 15',32144)
-# print(s.getCodigoDocente())
-# print(s.getDireccion())
-# s.setTelefono(32323256)
-# print(s.getTelefeno())
 
-class Bibliotecario():
+
+"""class Bibliotecario():  
     __ID=200
     def __init__(self,nom):
         Bibliotecario.__ID+=1
@@ -135,40 +127,32 @@ class Bibliotecario():
     def getCodigoBibliotecario(self):
         return Bibliotecario.__ID
 
-# b=Bibliotecario()
-# print(b.getCodigoBibliotecario())
-
-
-
 class Pedido():
-    def __init__(self,usu,ma,bi):
-        self.pedido=[]
-        self.__Usuario=usu
-        self.Material=ma
-        self.Bicliotecario=bi
-    def getUsuario(self):
-        return self.__Usuario
-    def getMaterial(self):
-        return self.Material
-    def getBibliotecario(self):
-        return self.Bicliotecario
-    def getTodo(self):
-        print(self.getUsuario())
-        print(self.getMaterial())
-        print(self.getBibliotecario())
+    def __init__(self, usu, ma, bi=0):
+        self.usu = usu
+        self.ma = ma
+        self.bi = bi
         
+    def getNombre(self):
+        print(self.usu)
+        print(self.ma)
+        #print(self.bi)
         
-
+    def Realizarpedido(self):
+        pass
+        """
+        
 
 y=libro('Cien Años de Soledad','Drama','Gabriel','NN')
+#print (y.Cont_Libro())
+
 x=Revista('chistes de la semana','comedia','perez','la risa')
+#print(x.Cont_Revis())
 
 m=Estudiante('Andres','cra 12', 321155164464)
-b=Bibliotecario('Maicol')
-print(b.getNombreBibliotecario())
-m.resevar(y)
-m.resevar(x)
-v=m.getRervados()
-ped=Pedido(m,v,b)
-#ped.getTodo()
-print(ped.getUsuario())
+m.reserva(x.getTitulo(), x.Cont_Revis())
+m.reserva(y.getTitulo(),y.Cont_Libro())
+print(m.getReserva())
+m.Entregar('Cien Años de Soledad')
+print(m.getReserva())
+#print(m.todo())
